dfb33cfb42db13cb5e7af0522e198783082cb680
Andre Rocha <steam228@steam228mac.local>
2018-11-24 15:36:55 +0000

    altera

--- Ex1e2/Ex1e2.ino
+++ Ex1e2/Ex1e2.ino
@@ -21,37 +21,52 @@ void setup() {
   calib();
   delay(5000);
   Serial.println ("Já Calibrou");
-  
- 
+
+
 }
 
 
 void loop() {
 
   valor = analogRead(moistSens);
-  valor = map(valor,Min,Max,0,100);
-  if (valor<20) digitalWrite(ledPin,HIGH);
-  else digitalWrite(ledPin,LOW);
+  valor = map(valor, Min, Max, 0, 100);
+  if (valor < 20) digitalWrite(ledPin, HIGH);
+  else digitalWrite(ledPin, LOW);
   Serial.println(valor);
   delay(1);        // delay in between reads for stability
 }
 
 void calib() {
-  for (int i = 0; i <nTeste; i++){
+  Serial.println ("calibraMAX");
+  delay(1000);
+  for (int i = 0; i < nTeste; i++) {
 
     valor = analogRead(moistSens);
     if (valor < Min) {
       Min = valor;
-      }
-    if (valor > Max){
-      Max = valor;
-      }
-    Serial.print(Min);
-    Serial.print (" | " );
-    Serial.print (Max);
+    }
+
+    Serial.print (Min);
     Serial.print (" | " );
     Serial.println (valor);
-      delay(10);
-    }
-  
+    delay(10);
   }
+  delay(1000);
+  Serial.println ("calibraMIN");
+  delay(1000);
+
+  for (int j = 0; j < nTeste; j++) {
+
+    valor = analogRead(moistSens);
+
+    if (valor > Max) {
+      Max = valor;
+    }
+
+    Serial.print(Max);
+    Serial.print (" | " );
+    Serial.println (valor);
+    delay(10);
+  }
+
+}
